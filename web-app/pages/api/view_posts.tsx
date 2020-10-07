3ba93d2d21f97a788637faad6dfcd6346f1a237d
jasonxia17 <jasonx3@illinois.edu>
2020-10-07 17:53:56 -0500

    Switch to mysql2 to allow for awaiting queries

--- web-app/pages/api/view_posts.tsx
+++ web-app/pages/api/view_posts.tsx
@@ -1,34 +1,21 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import { createConnection } from "mysql";
-
-const connection = createConnection({
-  host: "localhost",
-  user: "nodejs",
-  password: "password",
-  database: "cs411_project"
-});
-
-connection.connect(err => {
-  if (err) {
-    throw err;
-  }
-});
+import { createConnection } from "mysql2/promise";
 
 async function viewPostsHandler(
   req: NextApiRequest,
   res: NextApiResponse
 ): Promise<void> {
-  if (req.method === "GET") {
-    return new Promise((resolve, reject) => {
-      connection.query("SELECT * FROM Posts", (err, rows) => {
-        if (err) {
-          throw err;
-        }
+  const connection = await createConnection({
+    host: "localhost",
+    user: "nodejs",
+    password: "password",
+    database: "cs411_project"
+  });
+  // TODO: this should probably be a static global variable, not sure where to put it though.
 
-        res.status(200).json({ posts: rows });
-        return resolve();
-      });
-    });
+  if (req.method === "GET") {
+    const [rows, fields] = await connection.query("SELECT * FROM Posts");
+    res.status(200).json({ posts: rows });
   } else {
     res.status(405).end(`Method ${req.method} not allowed.`);
   }
