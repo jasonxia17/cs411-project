5a7c62dc5f2930fa22b7ecd10a704818f894a31a
jasonxia17 <jasonx3@illinois.edu>
2020-10-20 16:24:02 -0500

    Add Comments table to SQL database

---
+++ mysql/schemas/comments.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS Comments (
+    CommentId INT PRIMARY KEY AUTO_INCREMENT,
+    UserId INT, /* todo, foreign key */
+    PostId INT REFERENCES Posts(PostId),
+    PostTime DATETIME DEFAULT CURRENT_TIMESTAMP,
+    Body TEXT
+);
+
+INSERT IGNORE INTO Comments(CommentId, PostId, UserId, Body) VALUES (1, 2, 2, "First comment!");
