7346a384034a450e744ef231e14c68f6fa68a267
Elizabeth Wang <eywang1999@gmail.com>
2020-10-21 14:21:26 -0700

    Add Aand link edpoint for creating courses

--- web-app/pages/api/create_course.tsx
+++ web-app/pages/api/create_course.tsx
@@ -1,11 +1,17 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import { getConnection } from "../shared/sql_connection";
+import { createConnection } from "mysql2/promise";
 
-export default async function createCourseHandler(
+async function createCourseHandler(
   req: NextApiRequest,
   res: NextApiResponse
 ): Promise<void> {
-  const connection = await getConnection();
+  const connection = await createConnection({
+    host: "localhost",
+    user: "nodejs",
+    password: "password",
+    database: "cs411_project"
+  });
+  // TODO: this should probably be a static global variable, not sure where to put it though.
 
   if (req.method !== "POST") {
     res.status(405).end(`Method ${req.method} not allowed.`);
@@ -13,9 +19,11 @@ export default async function createCourseHandler(
   }
 
   await connection.execute(
-    "INSERT INTO Courses(Title, Semester) VALUES (?, ?)",
-    [req.body.title, req.body.semester]
+    "INSERT INTO Courses(CourseId, Title, Semester) VALUES (?, ?, ?)",
+    [req.body.courseId, req.body.title, req.body.semester]
   );
 
   res.status(200).end();
 }
+
+export default makePostHandler;
