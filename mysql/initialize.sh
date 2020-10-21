d228deaf04d62a03e63816a3f88e1770574b99ad
Elizabeth Wang <eywang1999@gmail.com>
2020-10-21 15:58:20 -0700

    Add dependency ordering

--- mysql/initialize.sh
+++ mysql/initialize.sh
@@ -10,4 +10,5 @@ echo "Creating database $DB_NAME..."
 cat create_database.sql | mysql -u $1 -p
 
 echo "Creating tables and running scripts..."
-cat ./schemas/*.sql ./scripts/*.sql | mysql -u $1 -p $DB_NAME
+# CourseId is used as a foreign key in posts and topics so we need to create the courses table first
+cat ./schemas/courses.sql ./schemas/posts.sql ./schemas/topics.sql ./scripts/*.sql | mysql -u $1 -p $DB_NAME
