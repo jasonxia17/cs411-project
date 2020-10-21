9b36eadd31e16fd362461c01f554b588cd119751
Elizabeth Wang <eywang1999@gmail.com>
2020-10-21 15:59:38 -0700

    Add course ID as foreign key

--- mysql/schemas/posts.sql
+++ mysql/schemas/posts.sql
@@ -1,10 +1,11 @@
 CREATE TABLE IF NOT EXISTS Posts (
     PostId INT PRIMARY KEY AUTO_INCREMENT,
     UserId INT, /* todo, foreign key */
+    CourseId INT REFERENCES Courses(CourseId),
     TopicId INT REFERENCES Topics(TopicId),
     Title VARCHAR(256),
     PostTime DATETIME DEFAULT CURRENT_TIMESTAMP,
     Body TEXT
 );
 
-INSERT IGNORE INTO Posts(PostId, UserId, TopicId, Title, Body) VALUES (1, 1, 1, "Hello World!", 'cout << "Hello World" << endl');
+INSERT IGNORE INTO Posts(PostId, UserId, CourseId, TopicId, Title, Body) VALUES (1, 1, 1, 1, "Hello World!", 'cout << "Hello World" << endl');
