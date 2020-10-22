304be8fa1352fd32664808f31300965489c35749
Elizabeth Wang <eywang1999@gmail.com>
2020-10-22 16:37:20 -0700

    Restrict topic viewing to only display topics within course

--- web-app/pages/view_topics.tsx
+++ web-app/pages/view_topics.tsx
@@ -1,12 +1,21 @@
 import React, { useState, useEffect } from "react";
 import Link from "next/link";
 import { useRouter } from "next/router";
+import { convertCourseIdToString } from "./shared/helper_utilities";
 
 export default function ViewTopicsPage(): JSX.Element {
   const [topics, setTopics] = useState([]);
+  const { query } = useRouter();
+
+  const courseId = convertCourseIdToString(query.courseId);
 
   useEffect(() => {
-    fetch("/api/view_topics")
+    fetch("/api/view_topics", {
+      method: "GET",
+      headers: {
+        courseId
+      }
+    })
       .then(res => res.json())
       .then(data => {
         setTopics(data.topics);
