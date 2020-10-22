d5989f14555290c51fce8b19b01a9a24ad070b3b
Elizabeth Wang <eywang1999@gmail.com>
2020-10-22 13:52:10 -0700

    Add homepage per course

--- web-app/pages/view_courses.tsx
+++ web-app/pages/view_courses.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import React, { useState, useEffect } from "react";
 
 export default function ViewCourses(): JSX.Element {
@@ -5,17 +6,13 @@ export default function ViewCourses(): JSX.Element {
 
   useEffect(() => {
     fetch("/api/view_courses")
-      .then(res => {
-          console.log("res: ", res);
-          return res.json();
-      })
+      .then(res => res.json())
       .then(data => {
         setCourses(data.courses);
       })
       .catch(reason => console.log(reason));
   }, []); // empty array => effect never needs to re-run.
 
-  console.log("courses: ", courses);
   return (
     <ul>
       {courses.map(course => (
@@ -23,6 +20,11 @@ export default function ViewCourses(): JSX.Element {
           <h2>
             Title: {course.Title}, Semester: {course.Semester}
           </h2>
+          <div>
+            <Link href="/view_posts">
+              <a className="posts_link">Go to {course.Title} forum!</a>
+            </Link>
+          </div>
         </li>
       ))}
     </ul>
