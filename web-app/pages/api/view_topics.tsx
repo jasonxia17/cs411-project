5bc5219537d08103aa5f7ced2008351c867269d2
Elizabeth Wang <eywang1999@gmail.com>
2020-10-22 16:37:20 -0700

    Restrict topic viewing to only display topics within course

--- web-app/pages/api/view_topics.tsx
+++ web-app/pages/api/view_topics.tsx
@@ -1,13 +1,23 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { getConnection } from "../shared/sql_connection";
+import { convertCourseIdToString } from "../shared/helper_utilities";
 
 async function viewTopicsHandler(
   req: NextApiRequest,
   res: NextApiResponse
 ): Promise<void> {
   const connection = await getConnection();
+
   if (req.method === "GET") {
-    const [rows] = await connection.query("SELECT * FROM Topics");
+    const courseIdAsInt = parseInt(
+      convertCourseIdToString(req.headers.courseid)
+    );
+
+    const [
+      rows
+    ] = await connection.query("SELECT * FROM Topics WHERE CourseId = ?", [
+      courseIdAsInt
+    ]);
     res.status(200).json({ topics: rows });
   } else {
     res.status(405).end(`Method ${req.method} not allowed.`);
