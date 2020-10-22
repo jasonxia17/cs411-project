b4d8a2d2058b7fc88f4bd9183d5861ea115f8bad
Elizabeth Wang <eywang1999@gmail.com>
2020-10-22 16:22:32 -0700

    Add logic to only get posts made within the class

--- web-app/pages/view_posts.tsx
+++ web-app/pages/view_posts.tsx
@@ -1,12 +1,28 @@
 import React, { useState, useEffect } from "react";
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 export default function ViewPostsPage(): JSX.Element {
   const [posts, setPosts] = useState([]);
+  const { query } = useRouter();
 
+  // CourseID can either be a string or an array of strings
+  // Let's always make it a string
+  let courseId = query.courseId as string;
+  if (Array.isArray(courseId)) {
+    courseId = courseId[0];
+  }
   useEffect(() => {
-    fetch("/api/view_posts")
-      .then(res => res.json())
+    fetch("/api/view_posts", {
+      method: "GET",
+      headers: {
+        courseId
+      }
+    })
+      .then(res => {
+        console.log(res);
+        return res.json();
+      })
       .then(data => {
         setPosts(data.posts);
       })
