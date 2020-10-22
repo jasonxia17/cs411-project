1dc9713f53f8c89716b7627a33d44e115fcdc90f
Elizabeth Wang <eywang1999@gmail.com>
2020-10-22 13:56:56 -0700

    Refactor connection boilerplate to use shared connection

--- web-app/pages/api/create_course.tsx
+++ web-app/pages/api/create_course.tsx
@@ -1,17 +1,11 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import { createConnection } from "mysql2/promise";
+import { getConnection } from "../shared/sql_connection";
 
 export default async function createCourseHandler(
   req: NextApiRequest,
   res: NextApiResponse
 ): Promise<void> {
-  const connection = await createConnection({
-    host: "localhost",
-    user: "nodejs",
-    password: "password",
-    database: "cs411_project"
-  });
-  // TODO: this should probably be a static global variable, not sure where to put it though.
+  const connection = await getConnection();
 
   if (req.method !== "POST") {
     res.status(405).end(`Method ${req.method} not allowed.`);
