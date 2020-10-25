a4dbfc696220bc3ad27028b4bc11a5af39194fe5
estelle0500 <estelle0500@gmail.com>
2020-10-25 22:42:04 +0800

    Protect API endpoints from unauthenticated users

--- web-app/pages/api/make_post.tsx
+++ web-app/pages/api/make_post.tsx
@@ -1,21 +1,24 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import { getSession } from "next-auth/client";
+import verifyAuthentication from "../shared/authentication_middleware";
 import { getConnection } from "../shared/sql_connection";
 
 async function makePostHandler(
   req: NextApiRequest,
   res: NextApiResponse
 ): Promise<void> {
-  const connection = await getConnection();
-
   if (req.method !== "POST") {
     res.status(405).end(`Method ${req.method} not allowed.`);
     return;
   }
 
-  await connection.execute("INSERT INTO Posts(Title, Body) VALUES (?, ?)", [
-    req.body.postTitle,
-    req.body.postBody
-  ]);
+  const session = await verifyAuthentication(req, res);
+  const connection = await getConnection();
+
+  await connection.execute(
+    "INSERT INTO Posts(UserId, Title, Body) VALUES (?, ?, ?)",
+    [session.user["id"], req.body.postTitle, req.body.postBody]
+  );
 
   res.status(200).end();
 }
