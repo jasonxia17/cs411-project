aa725a5ea14cb873fe355d834f3b63ef05b0f059
estelle0500 <estelle0500@gmail.com>
2020-11-01 15:26:09 +0800

    Add API endpoint to set user name

---
+++ web-app/pages/api/auth/set_user_name.ts
@@ -0,0 +1,25 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { getConnection } from "../../../shared/sql_connection";
+import verifyAuthentication from "../../../shared/authentication_middleware";
+
+async function setUserNameHandler(
+  req: NextApiRequest,
+  res: NextApiResponse
+): Promise<void> {
+  if (req.method !== "POST") {
+    res.status(405).end(`Method ${req.method} not allowed.`);
+    return;
+  }
+
+  const session = await verifyAuthentication(req, res);
+  const connection = await getConnection();
+
+  await connection.execute("UPDATE users SET name = ? WHERE id = ?", [
+    req.body.name,
+    session.user["id"]
+  ]);
+
+  res.status(200).end();
+}
+
+export default setUserNameHandler;
