ebc9cc552911c9f3fac664af3d743d12f4955cca
Elizabeth Wang <eyw3@illinois.edu>
2020-11-13 12:10:29 -0800

    Add join code as foreign key

--- mysql/schemas/courses.sql
+++ mysql/schemas/courses.sql
@@ -1,8 +1,9 @@
 CREATE TABLE IF NOT EXISTS Courses (
     CourseId INT PRIMARY KEY AUTO_INCREMENT,
     Title VARCHAR(256),
-    Semester VARCHAR(256)
+    Semester VARCHAR(256),
+    JoinCode VARCHAR(256)
 );
 
-INSERT IGNORE INTO Courses(CourseId, Title, Semester) VALUES (1, "CS 900", "FA2020");
-INSERT IGNORE INTO Courses(CourseId, Title, Semester) VALUES (2, "CS 126", "FA2021");
+INSERT IGNORE INTO Courses(CourseId, Title, Semester, JoinCode) VALUES (1, "CS 900", "FA2020", "avadog");
+INSERT IGNORE INTO Courses(CourseId, Title, Semester, JoinCode) VALUES (2, "CS 126", "FA2021", "walkodoggo");
