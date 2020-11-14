da95e096496c0f20b9c3922c85e3abfda3172f5a
Elizabeth Wang <eyw3@illinois.edu>
2020-11-13 16:41:08 -0800

    Add option for instructors to see roster

--- web-app/pages/api/course/[courseId].tsx
+++ web-app/pages/api/course/[courseId].tsx
@@ -11,7 +11,7 @@ async function courseInformationHandler(
     return;
   }
 
-  await verifyAuthentication(req, res);
+  const session = await verifyAuthentication(req, res);
   const connection = await getConnection();
 
   const [
@@ -20,7 +20,25 @@ async function courseInformationHandler(
     "SELECT * FROM Courses WHERE Courses.CourseId = ?",
     [req.query.courseId]
   );
-  res.status(200).json({ courseData: courseData[0] });
+
+  const userId = session.user["id"];
+  const [
+    joinedAsInstructor
+  ] = await connection.execute(
+    "SELECT * from Instructors WHERE InstructorId = ? and CourseId = ?",
+    [userId, req.query.courseId]
+  );
+  const isInstructor = JSON.parse(JSON.stringify(joinedAsInstructor)).length;
+
+  const [
+    joinedAsStudent
+  ] = await connection.execute(
+    "SELECT * from Students WHERE StudentId = ? and CourseId = ?",
+    [userId, req.query.courseId]
+  );
+  const isStudent = JSON.parse(JSON.stringify(joinedAsStudent)).length;
+
+  res.status(200).json({ courseData: courseData[0], isStudent, isInstructor });
 }
 
 export default courseInformationHandler;
