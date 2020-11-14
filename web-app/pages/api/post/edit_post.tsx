3297978f0aabac7c21c959f2f642f627e249b83c
estelle0500 <estelle0500@gmail.com>
2020-11-14 11:19:36 +0800

    Add authentication to new endpoints

--- web-app/pages/api/post/edit_post.tsx
+++ web-app/pages/api/post/edit_post.tsx
@@ -1,16 +1,18 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { getConnection } from "../../../shared/sql_connection";
+import verifyAuthentication from "../../../shared/authentication_middleware";
 
 async function editPostHandler(
   req: NextApiRequest,
   res: NextApiResponse
 ): Promise<void> {
-  const connection = await getConnection();
-
   if (req.method !== "POST") {
     res.status(405).end(`Method ${req.method} not allowed.`);
     return;
   }
+
+  await verifyAuthentication(req, res);
+  const connection = await getConnection();
 
   await connection.execute("UPDATE Posts SET Body = ? WHERE PostId = ?", [
     req.body.postBody,
