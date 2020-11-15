7394d2deda0438d42a0fb3ec364389fd3bb734a6
Elizabeth Wang <eyw3@illinois.edu>
2020-11-14 19:19:13 -0800

    Set join code as unique

--- mysql/schemas/courses.sql
+++ mysql/schemas/courses.sql
@@ -2,7 +2,8 @@ CREATE TABLE IF NOT EXISTS Courses (
     CourseId INT PRIMARY KEY AUTO_INCREMENT,
     Title VARCHAR(256),
     Semester VARCHAR(256),
-    JoinCode VARCHAR(256)
+    JoinCode VARCHAR(256),
+    UNIQUE(JoinCode)
 );
 
 INSERT IGNORE INTO Courses(CourseId, Title, Semester, JoinCode) VALUES (1, "CS 900", "FA2020", "avadog");
