0f0a9aa557dab559388163034d58918b215d4a8e
Elizabeth Wang <eyw3@illinois.edu>
2020-11-17 14:30:59 -0800

    Remove option to create custom join code

--- web-app/pages/api/create_course.tsx
+++ web-app/pages/api/create_course.tsx
@@ -14,20 +14,36 @@ export default async function createCourseHandler(
   const session = await verifyAuthentication(req, res);
   const connection = await getConnection();
 
-  const [
-    courseCollidingJoinCode
-  ] = await connection.query(
-    "SELECT CourseId FROM Courses WHERE JoinCode = ?",
-    [req.body.joinCode]
-  );
-  if (JSON.parse(JSON.stringify(courseCollidingJoinCode)).length > 0) {
-    res.status(401).end("Join code already belongs to a preexisting course");
-    return;
+  async function buildJoinCode() {
+    const JOIN_CODE_LENGTH = 5;
+    const [existingJoinCodeRows] = await connection.query(
+      "SELECT JoinCode FROM Courses"
+    );
+    const existingJoinCodes = new Set();
+    JSON.parse(JSON.stringify(existingJoinCodeRows)).map(joinCodeObj =>
+      existingJoinCodes.add(joinCodeObj.JoinCode)
+    );
+
+    function buildRandomString(): string {
+      // https://stackoverflow.com/questions/1349404/generate-random-string-characters-in-javascript
+      // Note: First 2 chars will always be 0. -- we want to avoid them
+      const asciiBase = 36;
+      return Math.random()
+        .toString(asciiBase)
+        .substring(2, JOIN_CODE_LENGTH + 2);
+    }
+    let joinCode = buildRandomString();
+    while (joinCode in existingJoinCodes) {
+      joinCode = buildRandomString();
+    }
+    return joinCode;
   }
+
+  const joinCode = await buildJoinCode();
 
   await connection.execute(
     "INSERT INTO Courses(Title, Semester, JoinCode) VALUES (?, ?, ?)",
-    [req.body.title, req.body.semester, req.body.joinCode]
+    [req.body.title, req.body.semester, joinCode]
   );
 
   // TODO This is a hacky approach that only works when we auto-incr the course id
