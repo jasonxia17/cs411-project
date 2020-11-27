ba7a42c57836105e5738d4a8af511620bc04b423
jasonxia17 <jasonx3@illinois.edu>
2020-11-27 17:09:29 -0600

    Set up neo4j

---
+++ web-app/shared/neo4j_connection.tsx
@@ -0,0 +1,11 @@
+import neo4j from "neo4j-driver";
+
+const driver = neo4j.driver(
+  process.env.GRAPHENEDB_BOLT_URL,
+  neo4j.auth.basic(
+    process.env.GRAPHENEDB_BOLT_USER,
+    process.env.GRAPHENEDB_BOLT_PASSWORD
+  )
+);
+
+export default driver;
