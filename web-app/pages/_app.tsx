47a8ac17ebc70f30d2b3ee85edcf121fdeccdce2
jasonxia17 <jasonx3@illinois.edu>
2020-11-29 17:15:48 -0600

    Install bootstrap

--- web-app/pages/_app.tsx
+++ web-app/pages/_app.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Provider } from "next-auth/client";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function App({ Component, pageProps }): JSX.Element {
   return (
