954400fe5ef71267103b062f7944594e90b5eeb7
jasonxia17 <jasonx3@illinois.edu>
2020-11-29 18:25:59 -0600

    Bootstrapify the Post component

--- web-app/components/Post.tsx
+++ web-app/components/Post.tsx
@@ -1,32 +1,43 @@
 import React from "react";
 import Link from "next/link";
+import Card from "react-bootstrap/Card";
 
 interface PostData {
   PostId: string;
-  UserId: string;
+  Username: string;
   Title: string;
   Body: string;
   NumComments?: number;
+  PostTime: string;
 }
 
 export default function Post({
   PostId,
-  UserId,
+  Username,
   Title,
   Body,
-  NumComments
+  NumComments,
+  PostTime
 }: PostData): JSX.Element {
+  const timestampString = new Date(PostTime).toLocaleString("en-US", {
+    dateStyle: "short",
+    timeStyle: "short"
+  });
   return (
-    <div>
-      <Link href={`/post/${PostId}`}>
-        <a style={{ color: "chocolate" }}>
-          <h2>
-            Post {PostId}: {Title}
-            {NumComments !== undefined && ` (${NumComments} Comments)`}
-          </h2>
-        </a>
-      </Link>
-      <p>{Body}</p>
-    </div>
+    <Card style={{ marginTop: 30, marginBottom: 30 }}>
+      <Card.Body>
+        <Card.Title>{Title}</Card.Title>
+        <Card.Subtitle className="mb-2 text-muted">
+          Posted by {Username}
+        </Card.Subtitle>
+        <Card.Text>{Body}</Card.Text>
+      </Card.Body>
+      <Card.Footer className="text-muted">
+        Posted on {timestampString} • {NumComments}{" "}
+        {NumComments === 1 ? "Comment" : "Comments"}
+      </Card.Footer>
+
+      <a href={`/post/${PostId}`} className="stretched-link"></a>
+    </Card>
   );
 }
