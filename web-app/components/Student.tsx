5690db26cdf872f18467d428b3045535d8e181a8
Elizabeth Wang <eyw3@illinois.edu>
2020-11-30 13:37:08 -0800

    Add display for preference list

--- web-app/components/Student.tsx
+++ web-app/components/Student.tsx
@@ -1,8 +1,10 @@
 import React from "react";
+import React from "react";
 
 interface StudentData {
   key: string;
   name: string;
+  preference_list: Array<string> | null;
   partner?: string;
   removeStudentFromRoster: (studentId: string) => Promise<void>;
 }
@@ -10,13 +12,28 @@ interface StudentData {
 export default function Student({
   key,
   name,
+  preference_list,
   partner,
   removeStudentFromRoster
 }: StudentData): JSX.Element {
+  console.log("partner: ", partner);
   return (
     <div>
       <div>
-        <h2>Name: {name}</h2>
+        <h2>{name}</h2>
+      </div>
+      <div>
+        {partner != null ? (
+          <h2>Partner: {partner}</h2>
+        ) : (
+          <h2>Not enough data to autogenerate partner</h2>
+        )}
+      </div>
+      <div>
+        <h2>Preference list:</h2>
+        {preference_list.map(user => (
+          <li key={user}>{user}</li>
+        ))}
       </div>
       <div
         style={{
@@ -26,7 +43,7 @@ export default function Student({
         <button
           style={{ cursor: "pointer" }}
           onClick={() => {
-            removeStudentFromRoster(id);
+            removeStudentFromRoster(key);
           }}
         >
           Remove student from course
