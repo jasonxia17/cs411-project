28408de1ec4b9780e9651d55b3d57c46e623acab
jasonxia17 <jasonx3@illinois.edu>
2020-11-29 19:33:20 -0600

    Restyle [topicId] page

--- web-app/pages/topic/[topicId].tsx
+++ web-app/pages/topic/[topicId].tsx
@@ -1,11 +1,16 @@
 import React, { useState, useEffect, Fragment } from "react";
 import { useRouter } from "next/router";
 import useProtectedRoute from "../../hooks/protected_route_hook";
-import Link from "next/link";
+import Post from "../../components/Post";
+
+import Alert from "react-bootstrap/Alert";
+import Button from "react-bootstrap/Button";
 
 export default function SingleTopicPage(): JSX.Element {
   const { query } = useRouter();
   const [posts, setPosts] = useState([]);
+  const [topicTitle, setTopicTitle] = useState("");
+
   const makePostsLink = `/topic/${query.topicId}/make_post`;
   useEffect(() => {
     const topicId = query.topicId;
@@ -17,6 +22,7 @@ export default function SingleTopicPage(): JSX.Element {
       .then(res => res.json())
       .then(data => {
         setPosts(data.posts);
+        setTopicTitle(data.topicTitle);
       })
       .catch(reason => console.log(reason));
   }, [query]); // query is an empty object initially; need to rerun effect when it's populated
@@ -27,26 +33,34 @@ export default function SingleTopicPage(): JSX.Element {
   }
 
   return (
-    <Fragment>
-      <div style={{ border: "1px solid black", marginBottom: 30 }}>
-        <ul>
-          {posts === null
-            ? "You must post in this topic before viewing other posts."
-            : posts.map(post => (
-                <li key={post.TopicId}>
-                  <h2>
-                    Post {post.PostId} by User {post.UserId}: {post.Title}
-                  </h2>
-                  <p>{post.Body}</p>
-                </li>
+    <div className="body-wrapper">
+      <div className="limit-width">
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            alignItems: "center"
+          }}
+        >
+          <h2 style={{ flex: 1 }}>Topic: {topicTitle}</h2>
+          <Button size="lg" href={makePostsLink}>
+            Make a post
+          </Button>
+        </div>
+        <div>
+          {posts === null ? (
+            <Alert variant="secondary" style={{ marginTop: 30 }}>
+              You must post in this topic before viewing other posts.
+            </Alert>
+          ) : (
+            <Fragment>
+              {posts.map(post => (
+                <Post key={post.PostId} {...post} />
               ))}
-        </ul>
+            </Fragment>
+          )}
+        </div>
       </div>
-      <div>
-        <Link href={makePostsLink}>
-          <a className="page_link">Make a post!</a>
-        </Link>
-      </div>
-    </Fragment>
+    </div>
   );
 }
